69c50d5856dd86ed5ccfb5f9359add569370b089
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-06-24 18:48:14 +0530

    Implement routing with Clerk authentication in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,37 @@
+import { SignIn, SignUp } from "@clerk/clerk-react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <h1>Welcome to the DevOps Pipeline Dashboard!</h1>
+            </ProtectedRoute>
+          }
+        />
+        {/* Clerk Pages */}
+        <Route
+          path="/sign-in"
+          element={
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+              <SignIn />
+            </div>
+          }
+        />
+        <Route
+          path="/sign-up"
+          element={
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+              <SignUp />
+            </div>
+          }
+        />
+      </Routes>
     </>
   );
 }
