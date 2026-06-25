de2e9f7967f0ba20d1036d08d3218466b4b7cba6
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-06-25 22:00:35 +0530

    Add user routes for creating a new user with authentication protection

---
+++ server/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import { createUser } from "../controllers/userController.js";
+
+import protect from "../middlewares/auth.js";
+
+const UserRouter = express.Router();
+
+UserRouter.post("/create", protect, createUser);
+
+export default UserRouter;
