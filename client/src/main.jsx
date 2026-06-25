cdd3926ceadf94c246bdf438d47257e288e4ada8
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-06-25 22:02:57 +0530

    Refactor main.jsx to wrap App component with UserProvider inside BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/UserContext.jsx";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <App />
-    </ClerkProvider>
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={clerkPubKey}>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </ClerkProvider>,
 );
