bc4e701c9ed34e7c91b90aa0e3bb931cf461d44b
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-06-25 22:01:02 +0530

    Add createUser function to handle user creation and authentication

---
+++ server/controllers/userController.js
@@ -0,0 +1,34 @@
+import { getAuth, clerkClient } from "@clerk/express";
+import User from "../models/user.model.js";
+
+export const createUser = async (req, res) => {
+  try {
+    const { userId } = getAuth(req);
+    
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    let user = await User.findById(userId);
+
+    if (user) {
+      return res.status(200).json(user); 
+    }
+
+    const clerkUser = await clerkClient.users.getUser(userId);
+
+    user = await User.create({
+      _id: userId,
+      name: (clerkUser.firstName || "") + " " + (clerkUser.lastName || ""),
+      email: clerkUser.emailAddresses[0].emailAddress,
+      image: clerkUser.imageUrl,
+    });
+     
+    
+    res.status(201).json(user);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
