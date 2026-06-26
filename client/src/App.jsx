370641459468ba1278483550c27f69572936aee7
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-06-26 21:53:09 +0530

    Refactor App component to use AppRouter for routing management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,9 @@
-import { SignIn, SignUp } from "@clerk/clerk-react";
-import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
+import AppRouter from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <h1>Welcome to the DevOps Pipeline Dashboard!</h1>
-            </ProtectedRoute>
-          }
-        />
-        {/* Clerk Pages */}
-        <Route
-          path="/sign-in"
-          element={
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-              <SignIn />
-            </div>
-          }
-        />
-        <Route
-          path="/sign-up"
-          element={
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-              <SignUp />
-            </div>
-          }
-        />
-      </Routes>
+      <AppRouter />
     </>
   );
 }
