d517c05b7f760e270c000d743b517464b6b07b00
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-28 22:06:51 +0530

    feat(backend):Update the jenkins routes with details and artifacts

--- server/routes/jenkinsRoutes.js
+++ server/routes/jenkinsRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import protect from '../middlewares/auth.js';
-import { getBuilds,getPipelineStages } from '../controllers/jenkinsController.js';
+import { getBuilds,getPipelineStages,getConsoleLogs , getBuildDetails,getArtifacts } from '../controllers/jenkinsController.js';
 
 const router = express.Router();
 
 router.use(protect);
 router.get("/:jobName/builds", getBuilds);
 router.get("/:jobName/stages", getPipelineStages);
+router.get("/:jobName/console", getConsoleLogs);
+// These are probably missing
+router.get("/:jobName/details", getBuildDetails);
+router.get("/:jobName/artifacts", getArtifacts);
 
 export default router;
